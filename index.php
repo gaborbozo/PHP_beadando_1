7e1f17d88c340fd47842b93494e86d07c17c1ae4
Bozó Gábor <bg.bozogabor@gmail.com>
2021-01-02 16:49:03 +0100

    Added embedded video

--- index.php
+++ index.php
@@ -31,13 +31,23 @@
                 <p>
                     Soma (stylized as SOMA) is a survival horror video game developed and published by Frictional Games. The game was released on 22 September 2015 for Microsoft Windows, OS X, Linux, PlayStation 4 and on 1 December 2017 on Xbox One.
                 </p>
+
+                <p>
+                Soma is a survival horror video game played from a first-person perspective. The player will encounter a number of creatures, which each embody an aspect of the game's themes. Soma primarily utilizes elements of psychological horror instead of conventional scares found in most video games within the genre. Throughout the game, the player will find a large array of clues, such as notes and audio tapes, which builds atmosphere and furthers the plot. Similar to most titles by Frictional Games, the player progresses through puzzle-solving, exploration, and the use of stealth;[8] the player may die if they fail to avoid monsters, although two years after the initial release, a "Safe Mode" has been added that keeps the monsters but stops them from killing the player.
+                </p>
+
+                <div class="prologuevideo">
+                    <iframe src="https://www.youtube.com/embed/BZTfi1jv-EE" frameborder="0" allowfullscreen></iframe>
+                </div>
+
+
                 <p>
                     Soma takes place on an underwater remote research facility that contains machinery which are exhibiting human characteristics, such as consciousness. Simon Jarrett, a fish-out-of-water protagonist, finds himself there under mysterious circumstances and embarks upon discovering its history, while trying to make sense of his predicament and potential future.
                 </p>
+
                 <p>
                     Soma's gameplay builds on the conventions established in the previous horror titles of Frictional Games, including an emphasis on stealthy evasion of threats, puzzle-solving and immersion. However, in a break with this tradition, it also de-emphasizes aspects such as inventory management in favour of a tighter focus on narrative. Soma received positive reviews from critics, who applauded its story and voice acting, although its enemy design and encounters received some criticism.
                 </p>
-
             </div>
         </div>
     </div>
