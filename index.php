3a389a1348c293778ae3943fa546855f348abed8
Bozó Gábor <bg.bozogabor@gmail.com>
2021-01-02 16:02:45 +0100

    index.html created, basic implementations

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta charset="UTF-8">
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+<link rel="stylesheet" href="./src/style/global.css">
+<link rel="stylesheet" href="./src/style/index.css">
+
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@1,400&display=swap" rel="stylesheet">
+
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Staatliches&display=swap" rel="stylesheet">
+
+<title>Soma</title>
+</head>
+<body>
+<div class="container-fluid">
+    <div class="row">
+        <div class="topnav">
+            <a class="active">Home</a>
+            <a href=""></a>
+            <a href=""></a>
+            <a href=""></a>
+        </div>
+
+        <div class="maintext">
+            <div class="prologue">
+                <p>
+                    Soma (stylized as SOMA) is a survival horror video game developed and published by Frictional Games. The game was released on 22 September 2015 for Microsoft Windows, OS X, Linux, PlayStation 4 and on 1 December 2017 on Xbox One.
+                </p>
+                <p>
+                    Soma takes place on an underwater remote research facility that contains machinery which are exhibiting human characteristics, such as consciousness. Simon Jarrett, a fish-out-of-water protagonist, finds himself there under mysterious circumstances and embarks upon discovering its history, while trying to make sense of his predicament and potential future.
+                </p>
+                <p>
+                    Soma's gameplay builds on the conventions established in the previous horror titles of Frictional Games, including an emphasis on stealthy evasion of threats, puzzle-solving and immersion. However, in a break with this tradition, it also de-emphasizes aspects such as inventory management in favour of a tighter focus on narrative. Soma received positive reviews from critics, who applauded its story and voice acting, although its enemy design and encounters received some criticism.
+                </p>
+
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
